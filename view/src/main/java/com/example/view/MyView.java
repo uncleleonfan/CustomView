88f5ea028014466329da7c2a2dd8037296c8d26a
Leon <526247082@qq.com>
2017-02-24 14:16:01 +0800

    onMeasure.

--- view/src/main/java/com/example/view/MyView.java
+++ view/src/main/java/com/example/view/MyView.java
@@ -18,9 +18,35 @@ public class MyView extends View {
         super(context, attrs);
     }
 
+    /**
+     * 在布局中的layout_width，layout_height表示向父容器申请的宽高
+     * 父容器拿到申请的宽高之后，会打包成对孩子宽高的期望
+     * 父容器会调用孩子的measure(int widthMeasureSpec, int heightMeasureSpec)
+     * measure->onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+     *
+     * @param widthMeasureSpec 父容器（这里即LinearLayout）对MyView宽度的期望 ,跟layout_width有关系
+     * @param heightMeasureSpec 父容器（这里即LinearLayout）对MyView高度的期望跟layout_height有关系
+     */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //widthMeasureSpec 32位二进制
+        //前两位表示模式
+        //public static final int UNSPECIFIED = 0 << MODE_SHIFT;父容器对孩子大小没有限制，孩子想多大就多大
+        //public static final int EXACTLY     = 1 << MODE_SHIFT;父容器对孩子有确切的大小要求，大小就是后30位
+        //public static final int AT_MOST     = 2 << MODE_SHIFT;父容器对孩子的最大值有要求，大小就是后30位
+        //后30位表示大小
+        //拆出模式
+        int mode = MeasureSpec.getMode(widthMeasureSpec);
+        //拆出大小
+        int size = MeasureSpec.getSize(widthMeasureSpec);
+        Log.d(TAG, "mode " + (mode>>30) + " size" + size);
+        //默认使用父容器对孩子的期望的宽高
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //onMeasure方法里面必须调用setMeasuredDimension();
+        //如果特殊需求，view需要自己控制自己的宽高而不管父容器对view的期望，则调用setMeasuredDimension
+        //setMeasuredDimension(50, 50);
+        //super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
         Log.d(TAG, "onMeasure: ");
     }
 
